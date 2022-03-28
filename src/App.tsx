0f3e08f47063734922f99ba756617ccb578af49a
Ömer Şahin <63051255+1sahinomer1@users.noreply.github.com>
2022-03-28 15:58:06 +0300

    change date lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { ThemeProvider } from 'styled-components';
 import { useState } from 'react';
 import { darkTheme, GlobalStyles, lightTheme } from 'theme';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+
 import { Provider } from 'react-redux';
 import { store } from 'store';
 import { Home } from 'pages';
@@ -9,12 +12,14 @@ function App() {
   const [theme, setTheme] = useState('light');
 
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
-        <GlobalStyles />
-        <Home theme={theme} setTheme={setTheme} />
-      </ThemeProvider>
-    </Provider>
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <Provider store={store}>
+        <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+          <GlobalStyles />
+          <Home theme={theme} setTheme={setTheme} />
+        </ThemeProvider>
+      </Provider>
+    </LocalizationProvider>
   );
 }
 
